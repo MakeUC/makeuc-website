2b99837d01ad3374cc64989e67d190a6d86152ed
Cat <luongcat162002@gmail.com>
2023-07-10 05:03:31 +0000

    work in progress - need to check

--- server/.keystone/config.js
+++ server/.keystone/config.js
@@ -214,6 +214,46 @@ var Registrant = (0, import_core.list)(addCompoundKey({
 
 // src/graphql/index.ts
 var extendGraphqlSchema = import_core2.graphql.extend((base) => ({
+  graphql: {
+    // Fill in statistics
+    statistics: import_core2.graphql.field({
+      type: base.object("Registrant"),
+      //Undefined --> Change in the future
+      args: { year: import_core2.graphql.arg({ type: import_core2.graphql.nonNull(import_core2.graphql.Int) }) },
+      async resolve(source, { year }, context) {
+        const registrants = await context.db.Registrant.findMany({
+          where: { registrationYear: { equals: year } }
+        });
+        const females = registrants.filter((registrant) => registrant.gender === "Female");
+        const femalePercentage = females.length / registrants.length * 100;
+        const schools = [...new Set(registrants.map((registrant) => registrant.schoolId))];
+        const countries = [...new Set(registrants.map((registrant) => registrant.country))];
+        const ethnicities = {
+          "Asian": registrants.filter((registrant) => registrant.ethnicity === "Asian").length,
+          "White": registrants.filter((registrant) => registrant.ethnicity === "White").length,
+          "Black or African American": registrants.filter((registrant) => registrant.ethnicity === "Black or African American").length,
+          "Hispanic or Latino": registrants.filter((registrant) => registrant.ethnicity === "Hispanic or Latino").length,
+          "Prefer not to answer": registrants.filter((registrant) => registrant.ethnicity === "Prefer not to answer").length
+        };
+        const educations = {
+          "Bachelor's": registrants.filter((registrant) => registrant.degree === "Bachelor's").length,
+          "Master's": registrants.filter((registrant) => registrant.degree === "Master's").length,
+          "Associate's": registrants.filter((registrant) => registrant.degree === "Associate's").length,
+          "Doctorate": registrants.filter((registrant) => registrant.degree === "Doctorate").length,
+          "High School": registrants.filter((registrant) => registrant.degree === "High School").length
+        };
+        let stats = {
+          countRegistrants: registrants.length,
+          femalePercent: femalePercentage,
+          countSchoolsRepresented: schools.length,
+          countCountriesRepresented: countries.length,
+          ethnicityBreakdown: ethnicities,
+          educationBreakdown: educations
+        };
+        return stats;
+      }
+    })
+  },
   mutation: {
     verifyRegistrant: import_core2.graphql.field({
       type: base.object("Registrant"),
--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -6,6 +6,53 @@ import type { Context } from ".keystone/types";
 
 
 export const extendGraphqlSchema = graphql.extend(base => ({
+  graphql: {
+    // Fill in statistics
+    statistics: graphql.field({
+      type: base.object("Registrant"), //Undefined --> Change in the future
+      args: { year: graphql.arg({ type: graphql.nonNull(graphql.Int) }) },
+      async resolve(source, { year }, context: Context) {
+        const registrants = await context.db.Registrant.findMany({
+          where: { registrationYear: { equals: year } },
+        });
+
+        const females = registrants.filter(registrant => registrant.gender === "Female");
+
+        const femalePercentage = ((females.length) / registrants.length) * 100;
+
+        const schools = [...new Set(registrants.map(registrant => registrant.schoolId))];
+
+        const countries = [...new Set(registrants.map(registrant => registrant.country))];
+
+        const ethnicities = {
+          "Asian": registrants.filter(registrant => registrant.ethnicity === "Asian").length,
+          "White": registrants.filter(registrant => registrant.ethnicity === "White").length,
+          "Black or African American": registrants.filter(registrant => registrant.ethnicity === "Black or African American").length,
+          "Hispanic or Latino": registrants.filter(registrant => registrant.ethnicity === "Hispanic or Latino").length,
+          "Prefer not to answer": registrants.filter(registrant => registrant.ethnicity === "Prefer not to answer").length,
+        };
+
+        const educations = {
+          "Bachelor's": registrants.filter(registrant => registrant.degree === "Bachelor's").length,
+          "Master's": registrants.filter(registrant => registrant.degree === "Master's").length,
+          "Associate's": registrants.filter(registrant => registrant.degree === "Associate's").length,
+          "Doctorate": registrants.filter(registrant => registrant.degree === "Doctorate").length,
+          "High School": registrants.filter(registrant => registrant.degree === "High School").length,
+        };
+
+        let stats = {
+          countRegistrants: registrants.length,
+          femalePercent: femalePercentage,
+          countSchoolsRepresented: schools.length,
+          countCountriesRepresented: countries.length,
+          ethnicityBreakdown: ethnicities,
+          educationBreakdown: educations,
+        };
+
+        return stats;
+      },
+    }),
+  },
   mutation: {
     verifyRegistrant: graphql.field({
       type: base.object("Registrant"),
