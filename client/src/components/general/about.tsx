99ebb836d9f1c529d84ec005ab8ead1bec605091
John Whiting <john@jwhiting.dev>
2023-10-05 02:59:09 +0000

    feat(client): added kinetic-vision to sponsors and added 1819 partnership text in about

--- client/src/components/general/about.tsx
+++ client/src/components/general/about.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Logo1819 from "~/assets/1819-logo.png";
 import Logo2020 from "~/assets/old-logos/2020.png";
 import Logo2021 from "~/assets/old-logos/2021.png";
 import Logo2022 from "~/assets/old-logos/2022.png";
+import UCIEEELogo from "~/assets/uc-ieee-logo.png";
 
 
 export function About() {
@@ -25,6 +27,21 @@ export function About() {
         Whether you are an experienced hacker or a complete beginner, you are welcome at MakeUC.
         We cannot wait to see what you all create!
       </p>
+      <hr className="border border-muted-foreground" />
+      <p className="text-center">
+        The MakeUC hackathon is brought to you in partnership with
+        <br />
+        <Link href="https://ieee.uc.edu" target="_blank" className="text-primary">IEEE@UC</Link> and the&nbsp;
+        <Link href="https://innovation.uc.edu/" target="_blank" className="text-primary">1819 Innovation Hub</Link>.
+      </p>
+      <div className="flex flex-col gap-8 justify-center items-center md:flex-row">
+        <Link href="https://ieee.uc.edu" target="_blank" className="text-primary">
+          <Image alt="IEEE@UC Logo" src={UCIEEELogo} width={300} />
+        </Link>
+        <Link href="https://innovation.uc.edu/" target="_blank" className="text-primary">
+          <Image alt="1819 Innovation Hub Logo" src={Logo1819} width={300} />
+        </Link>
+      </div>
       <hr className="border border-muted-foreground" />
       <h3 className="text-2xl font-bold tracking-wider text-center">Past Hackathons</h3>
       <div className="flex flex-col md:flex-row justify-stretch gap-8 h-full">
