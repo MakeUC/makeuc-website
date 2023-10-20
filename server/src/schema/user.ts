cc6b2e20e0c3e723e1222f88ae11cf8549994a1d
John Whiting <thejwhiting@gmail.com>
2023-10-20 21:12:40 +0000

    feat(server): added judging-related models to the schema

--- server/src/schema/user.ts
+++ server/src/schema/user.ts
@@ -1,15 +1,13 @@
 import { list } from "@keystone-6/core";
-import { password, text, timestamp } from "@keystone-6/core/fields";
+import { password, relationship, select, text, timestamp } from "@keystone-6/core/fields";
 
 import { allOperations, isAuthenticated } from "../auth/access";
-
 
 
 export const User = list({
   access: {
     operation: allOperations(isAuthenticated),
   },
-
   fields: {
     name: text({ validation: { isRequired: true } }),
     email: text({
@@ -17,8 +15,24 @@ export const User = list({
       isIndexed: "unique",
     }),
     password: password({ validation: { isRequired: true } }),
-    createdAt: timestamp({
-      defaultValue: { kind: "now" },
+    createdAt: timestamp({ defaultValue: { kind: "now" } }),
+    roles: select({
+      type: "enum",
+      defaultValue: "default",
+      options: [
+        { label: "Admin", value: "admin" },
+        { label: "Organizer", value: "organizer" },
+        { label: "Judge", value: "judge" },
+        { label: "Default", value: "default" },
+      ],
+    }),
+    registrations: relationship({
+      ref: "Registrant.user",
+      many: true,
+    }),
+    judgements: relationship({
+      ref: "Judgement.judge",
+      many: true,
     }),
   },
   graphql: {
