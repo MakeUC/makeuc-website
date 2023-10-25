3cc9a0ba4be6addfe9edfa04cebb0b511fc2f809
Elaine Mansour <mansour.elaine.1@gmail.com>
2023-10-25 01:25:50 +0000

    apply suggestions

--- client/src/features/live-site/constants/schedule.tsx
+++ client/src/features/live-site/constants/schedule.tsx
@@ -22,23 +22,23 @@ function createData(name: string, location: string, location_virt: string, start
 
 export const scheduleData: EventInfo[] = [
   createData("Check-In", "First Floor Main Desk", "Discord #verification Channel", "Nov 4 - 09:00 AM", "Nov 4 - 10:30 AM"),
-  createData("Sponsor Expo", "Second Floor Common Area", "Discord Sponsor Channels", "Nov 4 - 09:00 AM", "Nov 4 - 10:30 AM"),
+  createData("Sponsor Expo", "Second Floor Common Area", "Discord Sponsor VC", "Nov 4 - 09:00 AM", "Nov 4 - 10:30 AM"),
   createData("Opening Ceremony", "Room 230", "YouTube Stream", "Nov 4 - 10:30 AM", "Nov 4 - 11:15 AM"),
-  createData("Lunch", "Second Floor Common Area", "N/A", "Nov 4 - 11:15 AM", "Nov 4 - 12:00 PM"),
+  createData("Lunch", "Second Floor Common Area", "-", "Nov 4 - 11:15 AM", "Nov 4 - 12:00 PM"),
   createData("Hacking Begins", "-", "-", "Nov 4 - 11:15 AM", "Nov 5 - 11:30 AM"),
-  createData("Hackathon 101", "Second Floor Common Area", "Teams Meeting", "Nov 4 - 01:00 PM", "Nov 4 - 01:30 PM"),
-  createData("Team Formation", "Second Floor Common Area", "Teams Meeting", "Nov 4 - 01:30 PM", "Nov 4 - 02:00 PM"),
+  createData("Hackathon 101", "Second Floor Common Area", "Discord #hackathon-101 VC", "Nov 4 - 01:00 PM", "Nov 4 - 01:30 PM"),
+  createData("Team Formation", "Second Floor Common Area", "Discord #team-formation VC", "Nov 4 - 01:30 PM", "Nov 4 - 02:00 PM"),
   createData("Workshops (see below)","Esports Lab", "Teams Meeting", "Nov 4 - 02:00 PM", "Nov 4 - 05:00 PM"),  
-  createData("Dinner", "Second Floor Common Area", "N/A", "Nov 4 - 06:00 PM", "Nov 4 - 06:30 PM"),
-  createData("Super Smash Bros Tournament", "Esports Lab", "N/A", "Nov 4 - 07:00 PM", "Nov 4 - 08:00 PM"),
-  createData("MLH Bob Ross Contest", "Esports Lab", "Discord #mlh activites Channel", "Nov 4 - 08:00 PM", "Nov 4 - 09:00 PM"),
-  createData("Midnight Snack", "Second Floor Common Area", "N/A", "Nov 5 - 12:00 AM", "Nov 5 - 12:30 AM"),
-  createData("Board Games/Jackbox Games", "Second Floor Common Area", "Discord #game-room-1-vc Channel", "Nov 5 - 01:00 AM", "Nov 5 - 02:00 PM"),
-  createData("Sim Racing", "Second Floor Common Area", "N/A", "Nov 5 - 04:00 AM", "Nov 5 - 05:00 AM"),
-  createData("Stretching", "Common Area", "N/A", "Nov 5 - 05:00 AM", "Nov 5 - 05:30 AM"),
-  createData("Breakfast", "Second Floor Common Area", "N/A", "Nov 5 - 09:00 AM", "Nov 5 - 10:00 AM"),
-  createData("Hacking Ends", "-", "-", "-", "Nov 5 - 11:30 AM"),
-  createData("Live Judging", "Assigned Discord Channel", "Assigned Discord Channel", "Nov 5 - 12:00 PM", "Nov 5 - 03:00 PM"),
+  createData("Dinner", "Second Floor Common Area", "-", "Nov 4 - 06:00 PM", "Nov 4 - 06:30 PM"),
+  createData("Super Smash Bros Tournament", "Esports Lab", "-", "Nov 4 - 07:00 PM", "Nov 4 - 08:00 PM"),
+  createData("MLH Game", "Esports Lab", "Discord #mlh-vc VC", "Nov 4 - 08:00 PM", "Nov 4 - 09:00 PM"),
+  createData("Midnight Snack", "Second Floor Common Area", "-", "Nov 5 - 12:00 AM", "Nov 5 - 12:30 AM"),
+  createData("Board Games/Jackbox Games", "Second Floor Common Area", "Discord #game-room-1-vc VC", "Nov 5 - 01:00 AM", "Nov 5 - 02:00 AM"),
+  createData("Sim Racing", "Second Floor Common Area", "-", "Nov 5 - 04:00 AM", "Nov 5 - 05:00 AM"),
+  createData("Stretching", "Common Area", "-", "Nov 5 - 05:00 AM", "Nov 5 - 05:30 AM"),
+  createData("Breakfast", "Second Floor Common Area", "-", "Nov 5 - 09:00 AM", "Nov 5 - 10:00 AM"),
+  createData("Hacking Ends", "-", "-", "Nov 4 - 11:30 AM", "Nov 5 - 11:30 AM"),
+  createData("Live Judging", "Assigned Discord VC", "Assigned Discord VC", "Nov 5 - 12:00 PM", "Nov 5 - 03:00 PM"),
   createData("Closing Ceremony", "Room 230", "YouTube Stream", "Nov 5 - 05:00 PM", "Nov 5 - 06:00 PM"),
 ];
 
@@ -51,11 +51,11 @@ export const workshopData: EventInfo[] = [
 function ScheduleGrid({ name, location, location_virt, start, end }: EventInfo) {
   return (
     <TableRow className="border-b border-separate transition-colors bg-secondary">
-      <TableCell className="p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{name}</TableCell>
-      <TableCell className="p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{location}</TableCell>
-      <TableCell className="p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{location_virt}</TableCell>
-      <TableCell className="p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{start}</TableCell>
-      <TableCell className="p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{end}</TableCell>
+      <TableCell className="text-center p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{name}</TableCell>
+      <TableCell className="text-center p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{location}</TableCell>
+      <TableCell className="text-center p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{location_virt}</TableCell>
+      <TableCell className="text-center p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{start}</TableCell>
+      <TableCell className="text-center p-4 align-middle [&:has([role=checkbox])]:pr-0 border-white border-2">{end}</TableCell>
     </TableRow>
   );
 }
