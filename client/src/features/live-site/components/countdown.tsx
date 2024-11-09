17cfba8e5d52124dc0e01ffb5e6dadd5ee947ef0
Rai <62800649+menamerai@users.noreply.github.com>
2024-11-09 15:21:27 +0000

    fix(client): hacking start timer change

--- client/src/features/live-site/components/countdown.tsx
+++ client/src/features/live-site/components/countdown.tsx
@@ -13,8 +13,8 @@ interface TimeLeft {
   hasStarted?: boolean;
 }
 
-const startTime = new Date("2024-11-09T11:30:00-04:00");
-const endTime = new Date("2024-11-10T11:30:00-05:00");
+const startTime = new Date("2024-11-09T12:00:00-05:00");
+const endTime = new Date("2024-11-10T12:00:00-05:00");
 
 const msToTime = (ms: number): TimeLeft => {
   const seconds = Math.floor((ms / 1000) % 60),
