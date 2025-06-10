a7f888453776b70367c1ddb31e3e186510e95ca3
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-06-09 22:27:29 -0400

    add colors and fonts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -21,14 +21,21 @@ export default async function HomePage() {
           <div className="flex flex-col p-8 pt-48 md:flex-row justify-between items-center w-full max-w-7xl md:pt-8 gap-16">
             <div className="h-[15vh] md:hidden" />
             <div>
-              <Image src={MakeUCText} alt="MakeUC" width="1000" />
-              <div className="text-muted-foreground text-large mt-4 titillium-web-bold">November 9 - 10, 2024</div>
+              <div className="text-muted-foreground text-large mt-4 header-text">
+                <h1>MakeUC 2024</h1>
+                November 8-9th, 2025
+              </div>
 
               {/* <Link href="/registration" className="" tabIndex={-1}>
                 <Button className="flex gap-2 mt-4 titillium-web-bold" size="lg">Register Now!</Button>
               </Link> */}
               <Link href="/about" className="" tabIndex={-1}>
-                <Button className="flex gap-2 mt-4 titillium-web-bold" size="lg">View Hackathon Information</Button>
+                <Button
+                  className="flex gap-2 mt-4 titillium-web-bold"
+                  size="lg"
+                >
+                  View Hackathon Information
+                </Button>
               </Link>
             </div>
             <div>
@@ -40,34 +47,45 @@ export default async function HomePage() {
 
       <ViewportSection className="flex items-center justify-center py-8 bg-muted">
         <div className="flex flex-col gap-8 px-8 w-full max-w-5xl">
-          <h2 className="text-3xl font-bold tracking-wider text-center titillium-web-bold">About</h2>
+          <h2 className="text-3xl font-bold tracking-wider text-center titillium-web-bold">
+            About
+          </h2>
           <About />
         </div>
       </ViewportSection>
 
-
       <ViewportSection className="flex flex-col items-center justify-center py-8">
         <div className="px-8 w-full max-w-5xl">
-          <h2 className="text-3xl font-bold tracking-wider text-center mb-8 titillium-web-bold">Tracks</h2>
+          <h2 className="text-3xl font-bold tracking-wider text-center mb-8 titillium-web-bold">
+            Tracks
+          </h2>
         </div>
         <div className="p-8">
           <TrackGrid />
         </div>
       </ViewportSection>
 
-
       <ViewportSection className="flex items-center justify-center py-8 bg-muted">
         <div className="px-8 w-full max-w-5xl">
-          <h2 className="text-3xl font-bold tracking-wider text-center mb-8 titillium-web-bold">Frequently Asked Questions</h2>
+          <h2 className="text-3xl font-bold tracking-wider text-center mb-8 titillium-web-bold">
+            Frequently Asked Questions
+          </h2>
           <Faq />
         </div>
       </ViewportSection>
 
-
       <ViewportSection className="flex items-center justify-center py-8">
         <div className="px-8 w-full max-w-5xl">
-          <h2 className="text-3xl font-bold tracking-wider text-center mb-8 titillium-web-bold">Sponsors</h2>
-          <h3 className="text-center titillium-web-regular">Are you interested in sponsoring MakeUC? Please email us at <Link href="mailto:contact@makeuc.io" className="text-primary">contact@makeuc.io</Link>!</h3>
+          <h2 className="text-3xl font-bold tracking-wider text-center mb-8 titillium-web-bold">
+            Sponsors
+          </h2>
+          <h3 className="text-center titillium-web-regular">
+            Are you interested in sponsoring MakeUC? Please email us at{" "}
+            <Link href="mailto:contact@makeuc.io" className="text-primary">
+              contact@makeuc.io
+            </Link>
+            !
+          </h3>
           <br />
           <SponsorGridPlaceholder />
           {/* <div className="flex flex-col justify-center items-center gap-8 border-4 border-dashed border-muted text-muted-gray-foreground rounded mt-8 min-h-[200px] p-4"> */}
---
+++ client/src/components/general/stars.tsx
@@ -0,0 +1,9 @@
+export function Stars() {
+  return (
+    <>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </>
+  );
+}
