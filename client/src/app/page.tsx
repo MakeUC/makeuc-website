8f67beb0ff016aaa1efb37a17af169d67cb6e556
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-06-16 21:25:19 -0400

    add colors and fonts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,7 @@
-import { PlugZap } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 import Logo from "~/assets/logo.png";
-import MakeUCText from "~/assets/title.svg";
 import { About } from "~/components/general/about";
 import { Faq } from "~/components/general/faq";
 import { Button } from "~/components/ui/button";
@@ -15,13 +13,38 @@ import { TrackGrid } from "~/features/tracks";
 export default async function HomePage() {
   return (
     <main>
-      <section className="relative flex min-h-[calc(100vh-70px)]">
+      <section className="relative flex flex-col min-h-[calc(100vh-70px)]">
         <div className="absolute h-full w-full hero-image -z-10" />
-        <div className="flex flex-1 justify-center items-end">
-          <div className="flex flex-col p-8 pt-48 md:flex-row justify-between items-center w-full max-w-7xl md:pt-8 gap-16">
-            <div className="h-[15vh] md:hidden" />
-            <div>
-              <div className="text-muted-foreground text-large mt-4 header-text">
+        <div className="flex-1 flex flex-col">
+          <div className="flex-1 relative w-full">
+            <div className="flex-1 flex flex-row w-full h-full">
+              <div className="flex-[1] flex items-center justify-start p-5 md:p-8">
+                <Image
+                  src={require("~/assets/spacefight-left.gif")}
+                  alt="Spacefight Left Animation"
+                  width={0}
+                  height={0}
+                  sizes="25vw"
+                  className="object-contain w-40 p-15 animate-left-gif "
+                  priority
+                />
+              </div>
+              <div className="flex-[3] flex items-center justify-end p-4 md:p-8">
+                <Image
+                  src={require("~/assets/spacefight-right.gif")}
+                  alt="Spacefight Right Animation"
+                  width={0}
+                  height={0}
+                  sizes="75vw"
+                  className="object-contain w-full h-full max-w-[500px] animate-right-gif"
+                  priority
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col p-8 pt-8 md:flex-row justify-between items-center w-full max-w-7xl gap-16 mx-auto ">
+            <div className="flex flex-col items-center">
+              <div className="text-muted-foreground text-large mt-4 header-text text-center">
                 <h1>MakeUC 2024</h1>
                 November 8-9th, 2025
               </div>
@@ -87,7 +110,7 @@ export default async function HomePage() {
             !
           </h3>
           <br />
-          <SponsorGridPlaceholder />
+          {/* <SponsorGridPlaceholder />*/}
           {/* <div className="flex flex-col justify-center items-center gap-8 border-4 border-dashed border-muted text-muted-gray-foreground rounded mt-8 min-h-[200px] p-4"> */}
           {/* <span className="text-xl font-semibold text-center">There are no sponsors currently. Please check back in the future!</span> */}
           {/* <PlugZap size={48} strokeWidth={1} /> */}
