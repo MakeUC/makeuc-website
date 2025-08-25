f2c10b2693d01604523046faad7cb382c1cfe34d
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-08-25 16:54:55 -0400

    fix data path

--- server/src/express/api/countries.ts
+++ server/src/express/api/countries.ts
@@ -5,7 +5,7 @@ import type { Request, Response } from "express";
 
 
 export function countriesHandler(req: Request, res: Response) {
-  const filePath = path.resolve(__dirname, "../../../../data/countriesminified.json");
+  const filePath = path.resolve(__dirname, "data/countriesminified.json");
   const stream = fs.createReadStream(filePath, { encoding: "utf-8" });
   res.setHeader("Content-Type", "application/json");
   stream.pipe(res);
