2b2003c5dd928201c752a371467e6c524b486d04
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-09-22 20:10:59 -0400

    hardcode stats

--- client/src/components/general/stats.tsx
+++ client/src/components/general/stats.tsx
@@ -36,12 +36,23 @@ export function Stats({ year }: StatsProps) {
   const statisticsJson = useMemo(() => {
     if (!statisticsData?.statistics) return undefined;
     try {
-      const result = statistics.safeParse(JSON.parse(statisticsData.statistics));
+      const result = statistics.safeParse(
+        JSON.parse(statisticsData.statistics),
+      );
       if (!result.success) {
         // eslint-disable-next-line no-console
         console.error(result.error);
         return undefined;
       }
+      // TODO: temporarily hardcoded until 2026
+      result.data = {
+        countRegistrants: 560,
+        femalePercent: 25.54,
+        countSchoolsRepresented: 70,
+        countCountriesRepresented: 30,
+        ethnicityBreakdown: {},
+        educationBreakdown: {},
+      };
       return result.data;
     } catch (error) {
       // eslint-disable-next-line no-console
@@ -68,15 +79,28 @@ export function Stats({ year }: StatsProps) {
     <div className="p-6">
       {/* Top section */}
       <div className="flex flex-col gap-2 text-center">
-        <span className="text-4xl font-semibold">{statisticsJson.countRegistrants}</span>
-        <h3 className="text-lg text-muted-foreground titillium-web-bold">Total Registrants</h3>
+        <span className="text-4xl font-semibold">
+          {statisticsJson.countRegistrants}
+        </span>
+        <h3 className="text-lg text-muted-foreground titillium-web-bold">
+          Total Registrants
+        </h3>
       </div>
 
       {/* Middle grid */}
       <div className="grid grid-cols-1 md:grid-cols-[repeat(3,_1fr)] gap-4 my-8 p-2 justify-between">
-        <StatsCard value={statisticsJson.femalePercent.toFixed(2)} label="Female Attendance" />
-        <StatsCard value={statisticsJson.countSchoolsRepresented} label="Schools Represented" />
-        <StatsCard value={statisticsJson.countCountriesRepresented} label="Countries Represented" />
+        <StatsCard
+          value={statisticsJson.femalePercent.toFixed(2) + "%"}
+          label="Female Attendance"
+        />
+        <StatsCard
+          value={statisticsJson.countSchoolsRepresented}
+          label="Schools Represented"
+        />
+        <StatsCard
+          value={statisticsJson.countCountriesRepresented}
+          label="Countries Represented"
+        />
       </div>
 
       {/* Bottom charts section */}
@@ -89,11 +113,19 @@ export function Stats({ year }: StatsProps) {
   );
 }
 
-function StatsCard({ value, label }: { value: string | number; label: string }) {
+function StatsCard({
+  value,
+  label,
+}: {
+  value: string | number;
+  label: string;
+}) {
   return (
     <div className="flex flex-col gap-2 text-center">
       <span className="text-3xl font-semibold">{value}</span>
-      <h3 className="text-lg text-muted-foreground titillium-web-bold">{label}</h3>
+      <h3 className="text-lg text-muted-foreground titillium-web-bold">
+        {label}
+      </h3>
     </div>
   );
 }
