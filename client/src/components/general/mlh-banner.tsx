1def0ba085e4bfa93afbaee86bc2712a35920e9f
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-10-11 22:21:35 -0400

    Update mlh-banner.tsx

--- client/src/components/general/mlh-banner.tsx
+++ client/src/components/general/mlh-banner.tsx
@@ -6,12 +6,13 @@ export function MLHBanner() {
     <a
       id="mlh-trust-badge"
       className="block max-w-[100px] min-w-[60px] absolute right-0 w-[10vw]"
-      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
+      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
       target="_blank"
+      style={{ marginTop: "3.5px" }}
     >
       <Image
-        src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
-        alt="Major League Hacking 2024 Hackathon Season"
+        src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+        alt="Major League Hacking 2026 Hackathon Season"
         className="w-full"
         width={100}
         height={173}
